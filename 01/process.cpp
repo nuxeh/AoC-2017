7a931ec7b79a55b6f3771ae937dc28eac31f1de6
Edward Cragg <drq.11235@gmail.com>
2017-12-01 11:08:31 +0000

    01: get values from file hackily

--- 01/process.cpp
+++ 01/process.cpp
@@ -3,10 +3,29 @@
 
 using namespace std;
 
+int process_file_input(ifstream &input) {
+	cout << input.get() << endl;
+}
+
+
 int main(int argc, char** argv)
 {
 	if (!ifstream(argv[1])) {
 		cout << argv[0] << ": File not found." << endl;
 		exit(1);
 	}
+
+	ifstream input;
+	input.open(argv[1]);
+
+//	process_file_input(&input);
+
+	cout << int(input.get()) - 48 << endl;
+	cout << input.get() << endl;
+	cout << input.get() << endl;
+	cout << input.get() << endl;
+	cout << input.get() << endl;
+	cout << input.get() << endl;
+
+	input.close();
 }
