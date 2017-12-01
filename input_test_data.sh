f9c17df1ac44e495d16b4963c70ed896f6ec4849
Edward Cragg <drq.11235@gmail.com>
2017-12-01 12:21:59 +0000

    input_test_data: handle input directories and make result optional

--- input_test_data.sh
+++ input_test_data.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 count=0
-while read -p "input> " i && [[ $i != "q" ]] ; do
-	echo $i > t$count.txt
-	echo "$i -> t$count.txt"
+
+while read -p "test [a/b]> " t; do
+	if [[ $t == "a" ]] || [[ $t == "b" ]]; then
+		break
+	fi
+	echo "invalid test"
+done
+
+while read -p "input> " i && [[ $i != "q" ]]
+do
+	mkdir -p inputs_$t
+	path=inputs_$t/t$count.txt
+	echo $i > $path
+	echo "$i -> $path"
 
 	read -p "result> " r
-	echo $r > r$count.txt
-	echo "$r -> r$count.txt"
+
+	if [[ $r != "" ]]; then
+		path=inputs_$t/r$count.txt
+		echo $r > $path
+		echo "$r -> $path"
+	fi
 
 	count=$[count + 1]
 done
