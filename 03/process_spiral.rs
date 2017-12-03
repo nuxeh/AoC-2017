cc8d3fd2d1b8603f2b0de492b798bf29cae6d242
Edward Cragg <drq.11235@gmail.com>
2017-12-03 12:04:41 +0000

    03: refactor

--- 03/process_spiral.rs
+++ 03/process_spiral.rs
@@ -5,17 +5,17 @@ fn main() {
 	let w_space = 11; // Must be even
 
 	let mut vec: Vec<i32> = Vec::with_capacity(w_space*w_space);
-
 	vec.extend(iter::repeat(0).take(w_space*w_space));
 
 	let mut vec_2d = vec![vec![0; w_space]; w_space];
 
-	let mut pos = ((w_space-1)/2, (w_space-1)/2); // Start from centre coordinate
-	let mut pos_2d = ((w_space*w_space)-1) / 2;
+	let mut pos = ((w_space*w_space)-1) / 2;
+	let mut pos_2d = ((w_space-1)/2, (w_space-1)/2); // Start from centre coordinate
+
 	let mut index = 1;
 
-	vec[pos_2d] = index;
-	vec_2d[pos.0][pos.1] = index;
+	vec[pos] = index;
+	vec_2d[pos_2d.0][pos_2d.1] = index;
 
 	println!("{:?}", vec);
 	println!("{:?}", vec_2d);
