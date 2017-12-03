c6ebd45573d13e19284e52b37f21d930135717a7
Edward Cragg <drq.11235@gmail.com>
2017-12-03 11:27:50 +0000

    03: use a tuple for position

--- 03/process_spiral.rs
+++ 03/process_spiral.rs
@@ -1,9 +1,11 @@
 fn main() {
 
-	let w_space = 11;
-
-	//let a: [[u32; 10]; 10] = [[0; 10]; 10];
+	let w_space = 11; // Must be even
 
 	let mut vec: Vec<i32> = Vec::with_capacity(w_space*w_space);
+
+	let mut pos = ((w_space-1)/2, (w_space-1)/2); // Start from centre coordinate
+
+
 
 }
