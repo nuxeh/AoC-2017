7594926db2360f6457236ee76ca2c249ba7cbc94
Edward Cragg <drq.11235@gmail.com>
2017-12-03 14:29:40 +0000

    03: remove 2d array stuff, refactor *_2d to *

--- 03/process_spiral.rs
+++ 03/process_spiral.rs
@@ -15,18 +15,18 @@ fn main() {
 
 	let w_space = 11; // Must be even
 
-	let mut vec: Vec<i32> = Vec::with_capacity(w_space*w_space);
-	vec.extend(iter::repeat(0).take(w_space*w_space));
+//	let mut vec: Vec<i32> = Vec::with_capacity(w_space*w_space);
+//	vec.extend(iter::repeat(0).take(w_space*w_space));
+//	let mut pos = ((w_space*w_space)-1) / 2;
+//	vec[pos] = index;
 
-	let mut vec_2d = vec![vec![0; w_space]; w_space];
+	let mut vec = vec![vec![0; w_space]; w_space];
 
-	let mut pos = ((w_space*w_space)-1) / 2;
-	let mut pos_2d = ((w_space-1)/2, (w_space-1)/2); // Start from centre coordinate
+	let mut pos = ((w_space-1)/2, (w_space-1)/2); // Start from centre coordinate
 
 	let mut index = 1;
 
-	vec[pos] = index;
-	vec_2d[pos_2d.0][pos_2d.1] = index;
+	vec[pos.0][pos.1] = index;
 
 	let mut dir = Direction::Right;
 	let mut next_dir: Direction;
@@ -34,27 +34,27 @@ fn main() {
 
 	for x in 1..10 {
 
-		vec_2d[pos_2d.0][pos_2d.1] = x;
+		vec[pos.0][pos.1] = x;
 
-		next_pos = get_next_pos(&pos_2d, &dir);
+		next_pos = get_next_pos(&pos, &dir);
 
-		println!("{}", vec_2d[next_pos.0][next_pos.1]);
+		println!("{}", vec[next_pos.0][next_pos.1]);
 		println!("{:?}", dir);
 
-		if vec_2d[next_pos.0][next_pos.1] == 0 {
+		if vec[next_pos.0][next_pos.1] == 0 {
 			dir = get_next_dir(&dir);
-			pos_2d = next_pos;
+			pos = next_pos;
 		} else {
-			pos_2d = get_next_pos(&pos_2d, &dir);
+			pos = get_next_pos(&pos, &dir);
 		}
 
 		println!("{:?}", dir);
 
-		print_space(&vec_2d);
+		print_space(&vec);
 		println!("");
 	}
 
-	print_space(&vec_2d);
+	print_space(&vec);
 }
 
 fn get_next_dir (cur: &Direction) -> Direction {
