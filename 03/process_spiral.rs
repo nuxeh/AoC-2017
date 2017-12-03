42e1eed1a3f2a89a2a776639bd5ff2f959df259c
Edward Cragg <drq.11235@gmail.com>
2017-12-03 11:41:03 +0000

    03: add some variables, use an iterator to fill vec with zeroes

--- 03/process_spiral.rs
+++ 03/process_spiral.rs
@@ -1,11 +1,20 @@
+use std::iter;
+
 fn main() {
 
 	let w_space = 11; // Must be even
 
 	let mut vec: Vec<i32> = Vec::with_capacity(w_space*w_space);
 
+	vec.extend(iter::repeat(0).take(w_space*w_space));
+
+	println!("{:?}", vec);
+
 	let mut pos = ((w_space-1)/2, (w_space-1)/2); // Start from centre coordinate
+	let mut pos_2d = ((w_space*w_space)-1) / 2;
+	let mut index = 1;
 
+	vec[pos_2d] = index;
 
-
+	println!("{:?}", vec);
 }
