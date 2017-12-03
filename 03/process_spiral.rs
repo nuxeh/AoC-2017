ac739a8f37fab2a6fd776fe5a6eb978a62bea3a2
Edward Cragg <drq.11235@gmail.com>
2017-12-03 11:58:10 +0000

    03: set start index based on 2d position

--- 03/process_spiral.rs
+++ 03/process_spiral.rs
@@ -8,17 +8,16 @@ fn main() {
 
 	vec.extend(iter::repeat(0).take(w_space*w_space));
 
-	println!("{:?}", vec);
-
 	let mut vec_2d = vec![vec![0; w_space]; w_space];
-
-	println!("{:?}", vec_2d);
 
 	let mut pos = ((w_space-1)/2, (w_space-1)/2); // Start from centre coordinate
 	let mut pos_2d = ((w_space*w_space)-1) / 2;
 	let mut index = 1;
 
 	vec[pos_2d] = index;
+	vec_2d[pos.0][pos.1] = index;
 
 	println!("{:?}", vec);
+	println!("{:?}", vec_2d);
+
 }
