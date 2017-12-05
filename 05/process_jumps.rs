864acb08936815d16e0fc267b9262d6f7391ef99
Edward Cragg <drq.11235@gmail.com>
2017-12-05 11:13:48 +0000

    05: tidy up a bit

--- 05/process_jumps.rs
+++ 05/process_jumps.rs
@@ -3,34 +3,25 @@ use std::fs::File;
 use std::io::prelude::*;
 
 fn main() {
-    let filename = "input.txt";
+	let filename = "input.txt";
 
-    println!("In file {}", filename);
+	println!("In file {}", filename);
 
-    let mut f = File::open(filename).expect("file not found");
+	let mut f = File::open(filename).expect("file not found");
 
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("something went wrong reading the file");
+	let mut contents = String::new();
+	f.read_to_string(&mut contents)
+		.expect("something went wrong reading the file");
 
-//    println!("With text:\n{}", contents);
-
-
-//	to_arr(&contents);
-//}
-
-
-//fn to_arr (str: &String) {
+	/* TODO pass reference to string to function? */
 	let mut split = contents.split("\n");
-
-//	println!("{}", str);
 
 	let mut vec = Vec::new();
 	for s in split {
 		let n = s.parse::<i32>().unwrap();
 
 		println!("{}", n);
-		vec.push(s.parse::<i32>().unwrap())
+		vec.push(n);
 	}
 
 	println!("{:?}", vec);
