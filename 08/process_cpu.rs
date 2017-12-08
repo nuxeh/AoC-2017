4111b75430b48588c4d497b4e140d19495c8e7ea
Edward Cragg <drq.11235@gmail.com>
2017-12-08 06:56:22 +0000

    08: initial implementation and data

---
+++ 08/process_cpu.rs
@@ -0,0 +1,64 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+use std::collections::HashMap;
+use std::iter::Iterator;
+
+fn main() {
+//	let filename = "input.txt";
+	let filename = "test.txt";
+
+	let mut f = File::open(filename).expect("file not found");
+
+	let mut contents = String::new();
+	f.read_to_string(&mut contents)
+		.expect("something went wrong reading the file");
+
+	/* TODO pass reference to string to function? */
+	let split = contents.split(|c| c == '\n');
+
+//	let mut elems = HashMap::new();
+	let mut r = HashMap::new();
+
+//	let mut vec = Vec::new();
+	for s in split {
+		let words = s.split(|c| c == ' ');
+
+//		let n = words.nth(1).unwrap().parse::<u32>();
+//		match n {
+//			Ok(n)     => {vec.push(n)}
+//			Err(_err) => {continue}
+//		}
+
+		println!("{}", s);
+		let mut reg = "";
+		let mut oper;
+		let mut diff;
+		let mut comp1;
+		let mut comp2;
+		let mut comp_op;
+
+		for (i, w) in words.enumerate() {
+
+			match i {
+				0 => { reg  = w; },
+				1 => { oper = w; },
+				2 => { diff = w.parse::<u32>().unwrap(); },
+				4 => { comp1 = w; },
+				5 => { comp_op = w; },
+				6 => { comp2 = w; },
+				_ => {}
+			}
+
+			println!("{}", w);
+		}
+			r.entry(reg.clone()).or_insert(0);
+//				r.insert
+
+
+	}
+
+//	println!("initial array: {:?}", vec);
+//	println!("weights: {:?}", weights);
+
+}
