e4e3e80314ff183f65c03f369a706c772ca7002d
Edward Cragg <drq.11235@gmail.com>
2017-12-10 08:14:03 +0000

    10: solve part 1

--- 10/process_hash.py
+++ 10/process_hash.py
@@ -1,6 +1,7 @@
 lengths = [97,167,54,178,2,11,209,174,119,248,254,0,255,1,64,190]
-lengths = [3, 4, 1, 5]
-llength = 4
+#lengths = [3, 4, 1, 5]
+llength = 5
+llength = 256
 
 pos = 0
 skip = 0
@@ -9,19 +10,24 @@ a = [x for x in range(llength)]
 
 print(a)
 
-#b = [x for x in 
 for length in lengths:
+	print length
 	b = [0] * length
 	for i in range(length):
 		b[i] = a[(pos + i) % llength]
 
+	print "b ", b
 	b.reverse()
 
 	for i in range(length):
 		a[(pos + i) % llength] = b[i]
 
-	print length
-	print b
+	print "b ", b
+	print "a ", a
 
-	pos += (pos + length + skip) % llength
+	pos = (pos + length + skip) % llength
 	skip += 1
+
+	print skip, pos
+
+print "result is", a[0] * a[1]
