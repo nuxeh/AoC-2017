9a6431db2c0f7d70e9c09cf3a0239c8a2a2ae64d
Edward Cragg <drq.11235@gmail.com>
2017-12-10 07:50:33 +0000

    10: iterate lengths

--- 10/process_hash.py
+++ 10/process_hash.py
@@ -1,3 +1,16 @@
-a = [x for x in range(255)]
+lengths = [97,167,54,178,2,11,209,174,119,248,254,0,255,1,64,190]
+lengths = [3, 4, 1, 5]
+llength = 4
+
+pos = 0
+skip = 0
+
+a = [x for x in range(llength)]
 
 print(a)
+
+#b = [x for x in 
+
+for length in lengths:
+	b = 
+	print length
