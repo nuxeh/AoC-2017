66fa9b1b2b1a3ac581dc3a1810a20498670aafa3
Edward Cragg <drq.11235@gmail.com>
2017-12-10 08:39:29 +0000

    10: recreate tests as function calls

--- 10/process_hash.py
+++ 10/process_hash.py
@@ -1,21 +1,15 @@
 lengths = [97,167,54,178,2,11,209,174,119,248,254,0,255,1,64,190]
-#lengths = [3, 4, 1, 5]
-llength = 5
-llength = 256
-
-pos = 0
-skip = 0
-
-a = [x for x in range(llength)]
-
-print(a)
+lengths_t = [3, 4, 1, 5]
 
 def get_ascii(string):
 	a = [ord(c) for c in string]
 	return a + [17, 31, 73, 47, 23]
 
-def do_hash(arr):
-	global pos, skip
+def do_hash(arr, llength=256):
+	pos = 0
+	skip = 0
+	a = [x for x in range(llength)]
+	print a
 	for length in arr:
 		b = [0] * length
 		for i in range(length):
@@ -29,8 +23,10 @@ def do_hash(arr):
 		pos = (pos + length + skip) % llength
 		skip += 1
 
+	print "result is", a[0] * a[1]
+
+do_hash(lengths_t, 5)
 do_hash(lengths)
 
 
-print "result is", a[0] * a[1]
 print get_ascii("1,2,3")
