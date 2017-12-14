a3ed22102994aacfdc0178192233f1e197dcb7ff
Edward Cragg <drq.11235@gmail.com>
2017-12-14 11:42:54 +0000

    13: add test function

--- 13/process_firewall.rs
+++ 13/process_firewall.rs
@@ -3,3 +3,26 @@ fn main()
 	let mut a = vec![0; 10];
 	println!("{:?}", a)
 }
+
+fn test_positions(r, n)
+{
+	let mut p = 0;
+	let mut d = 1;
+	let mut r = 1;
+
+	for (n=0; n<max; n++) {
+		let mut s = "[";
+		for (x=0; x<l; x++) {
+			if (x == p)
+				s = concat!(s, "*");
+			else
+				s = concat!(s, " ");
+		}
+		println!(concat(s, "]", p));
+
+		if		(d > 0 && p == r - 1)	{d *= -1}
+		else if		(d < 0 && p == 0)	{d *= -1}
+		p += d;
+	}
+
+}
