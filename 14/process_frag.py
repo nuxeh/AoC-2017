23f8d6499755fe33446817b92025df043855aaec
Edward Cragg <drq.11235@gmail.com>
2017-12-14 09:20:30 +0000

    14: get correct length bin rep

--- 14/process_frag.py
+++ 14/process_frag.py
@@ -7,7 +7,7 @@ t = knot_hash_string("flqrgnkx")
 def hash_to_bit_map(hash_string):
 	ret = ""
 	for c in hash_string:
-		ret += '{0:b}'.format(int(c, 16))
+		ret += '{0:04b}'.format(int(c, 16))
 	return ret
 
 print t
