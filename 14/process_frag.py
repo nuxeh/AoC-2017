e55cb61fa764680ab1c8ae00705adc29c796d99f
Edward Cragg <drq.11235@gmail.com>
2017-12-14 09:14:59 +0000

    14: play

--- 14/process_frag.py
+++ 14/process_frag.py
@@ -3,3 +3,12 @@
 from process_hash import knot_hash_string
 
 print knot_hash_string("flqrgnkx")
+
+print ord("e")
+print int("e", 16)
+print '{0:b}'.format(int("e", 16))
+
+for c in "aoc2017":
+	print ord(c)
+#	print int(c)
+#	print '{0:b}'.format(c)
