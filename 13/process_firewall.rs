5b5192620a9a36c527e091b7a5a6c9d1936a86a2
Edward Cragg <drq.11235@gmail.com>
2017-12-14 12:08:14 +0000

    13: print offset 0 test
        $ ./process_firewall
        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
        [*         ] 0 0
        [ *        ] 1 1
        [  *       ] 2 2
        [   *      ] 3 3
        [    *     ] 4 4
        [     *    ] 5 5
        [      *   ] 6 6
        [       *  ] 7 7
        [        * ] 8 8
        [         *] 9 9
        [        * ] 8 10
        [       *  ] 7 11
        [      *   ] 6 12
        [     *    ] 5 13
        [    *     ] 4 14
        [   *      ] 3 15
        [  *       ] 2 16
        [ *        ] 1 17
        [*         ] 0 0
        [ *        ] 1 1
        [  *       ] 2 2
        [   *      ] 3 3
        [    *     ] 4 4
        [     *    ] 5 5

--- 13/process_firewall.rs
+++ 13/process_firewall.rs
@@ -3,7 +3,7 @@ fn main()
 	let mut a = vec![0; 10];
 	println!("{:?}", a);
 
-	test_positions(10, 14);
+	test_positions(10, 24);
 }
 
 fn test_positions(_r: u32, _n: u32)
@@ -11,7 +11,7 @@ fn test_positions(_r: u32, _n: u32)
 	let mut p: i32 = 0;
 	let mut d: i32 = 1;
 
-	for _ in 0.._n {
+	for n in 0.._n {
 		let mut s = "[".to_string();
 		for x in 0.._r {
 			if x == p as u32 {
@@ -20,11 +20,13 @@ fn test_positions(_r: u32, _n: u32)
 				s = s + " ";
 			}
 		}
-		println!("{}{}", s + "] ", p);
+
+		let test = n % ((_r - 1) * 2);
+
+		println!("{} {} {}", s + "]", p, test);
 
 		if		d > 0 && p == _r as i32 - 1	{d *= -1}
 		else if		d < 0 && p == 0			{d *= -1}
 		p += d;
 	}
-
 }
