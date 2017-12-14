25669d7f7fcde83027ba9cb54fb376a5efa4b1b3
Edward Cragg <drq.11235@gmail.com>
2017-12-14 11:58:11 +0000

    13: update test function (finish rustifying)
        $ ./process_firewall
        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
        [*         ] 0
        [ *        ] 1
        [  *       ] 2
        [   *      ] 3
        [    *     ] 4
        [     *    ] 5
        [      *   ] 6
        [       *  ] 7
        [        * ] 8
        [         *] 9
        [        * ] 8
        [       *  ] 7
        [      *   ] 6
        [     *    ] 5

--- 13/process_firewall.rs
+++ 13/process_firewall.rs
@@ -1,28 +1,29 @@
 fn main()
 {
 	let mut a = vec![0; 10];
-	println!("{:?}", a)
+	println!("{:?}", a);
+
+	test_positions(10, 14);
 }
 
 fn test_positions(_r: u32, _n: u32)
 {
-	let mut p = 0;
-	let mut d = 1;
-	let mut r = 1;
+	let mut p: i32 = 0;
+	let mut d: i32 = 1;
 
-	for n in 0.._n {
-		let mut s = "[";
+	for _ in 0.._n {
+		let mut s = "[".to_string();
 		for x in 0.._r {
-			if (x == p) {
+			if x == p as u32 {
 				s = s + "*";
 			} else {
 				s = s + " ";
 			}
 		}
-		println!("{}", s + "]" + p);
+		println!("{}{}", s + "] ", p);
 
-		if		(d > 0 && p == r - 1)	{d *= -1}
-		else if		(d < 0 && p == 0)	{d *= -1}
+		if		d > 0 && p == _r as i32 - 1	{d *= -1}
+		else if		d < 0 && p == 0			{d *= -1}
 		p += d;
 	}
 
