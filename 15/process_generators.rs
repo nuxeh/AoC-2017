8a77b23f0618afba20041e793dbe1baf5a980378
Edward Cragg <drq.11235@gmail.com>
2017-12-15 10:24:22 +0000

    15: get solution for part 1

--- 15/process_generators.rs
+++ 15/process_generators.rs
@@ -3,17 +3,31 @@ fn main ()
 	let mut a: u64 = 65;
 	let mut b: u64 = 8921;
 
+	a = 289;
+	b = 629;
+
 	let f_a = 16807;
 	let f_b = 48271;
 
 	let d = 2147483647;
 
-	for i in 0..5 {
+	let mut count = 0;
+
+	for i in 0..40000000 {
 		a = (a * f_a) % d;
 		b = (b * f_b) % d;
 
-		println!("{} {}", a, b)
+		if a as u16 == b as u16 {
+			println!("match: {:b} {:b}", a as u16, b as u16);
+			//println!("{:b}", a);
+			//println!("{:b}", b);
+			count += 1;
+		}
+
+//		println!("{} {}", a, b)
 
 	}
+
+	println!("{}", count);
 }
 
