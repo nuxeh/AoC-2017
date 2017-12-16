cc971a46843e1d284b321454e19c783923f56470
Edward Cragg <drq.11235@gmail.com>
2017-12-16 12:56:28 +0000

    Add rust template script

---
+++ rust_template.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#
+# Create rust template
+
+if [[ $# -ne 2 ]]; then
+	printf "usage:\n    $0 [name] [day-number]\n"
+	exit 1
+fi
+
+cat > "$1.rs" << EOF
+fn main () {
+
+}
+EOF
+
+cat > .git_hook << EOF
+#!/bin/bash
+#
+# Git commit prepare hook
+
+echo "# Advent of code!" >> \$1
+D=$2
+sed -i "1i\$D: " \$1
+EOF
+
+echo '.git_hook' >> .gitignore
+
+cat > Makefile << EOF
+day=$2
+
+$1: \$@.rs
+	rustc \$@.rs
+
+all: $1
+	./$1
+
+
+\$(hook): .git_hook
+	cat .git_hook > \$@
+	chmod +x \$@
+
+commit: \$(hook)
+	git commit
+EOF
+
+git add Makefile $1.rs .gitignore
+git ct -m "$2: add rust template"
