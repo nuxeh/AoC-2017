f39fbae9a7efbe6ded5d33b133ae46e0adcbe9a3
Edward Cragg <drq.11235@gmail.com>
2017-12-16 16:11:57 +0000

    16: wip command parsing

--- 16/dance.rs
+++ 16/dance.rs
@@ -23,9 +23,28 @@ fn main () {
 		}
 	}
 
+	let moves2: Vec<(String, u32, String, String, u32, u32)>;
+
 	for m in moves {
 		println!("{:?}", m);
+
 		// Rust strings are UTF-8 and can't be indexed
 		println!("{:?}", m.get(0..1).unwrap());
+
+		let cmd = m.get(0..1).unwrap();
+
+		match cmd {
+			"s" => {
+				let n: u32 = m.get(1..2).unwrap().parse().unwrap();
+				moves2.push((cmd, n, "", "", 0, 0));
+			}
+			"x" => {
+
+			}
+			"p" => {
+
+			}
+			_ => {}
+		}
 	}
 }
