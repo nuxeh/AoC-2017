a6fdaa3a51a7c16e96d97775a63889606f0b2a49
Edward Cragg <drq.11235@gmail.com>
2017-12-16 13:55:50 +0000

    16: read lines from stdin and split by commas
        $ echo fooey,fooey1,fooey2 | ./dance
        ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P']
        "fooey"
        "fooey1"
        "fooey2"

--- 16/dance.rs
+++ 16/dance.rs
@@ -1,7 +1,22 @@
+use std::io;
+use std::io::BufRead;
+
 fn main () {
 
 	let mut c: u8 = 64;
 	let mut a: Vec<_> = vec![0; 16]
 		.iter().map(|_| {c += 1; c as char}).collect();
 	println!("{:?}", a);
+
+
+	let stdin = io::stdin();
+
+	for line in stdin.lock().lines() {
+		let line = line.unwrap();
+		let split: Vec<_> = line.split(",").collect();
+
+		for m in split {
+			println!("{:?}", m);
+		}
+	}
 }
