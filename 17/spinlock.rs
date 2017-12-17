95f194fdeb42c2999632d1ec308a92a85daa55d3
Edward Cragg <drq.11235@gmail.com>
2017-12-17 13:47:48 +0000

    17: print final values for test data

--- 17/spinlock.rs
+++ 17/spinlock.rs
@@ -6,19 +6,19 @@ fn main () {
 	let mut buf = vec![0];
 	let mut p = 0;
 
-	println!("{:?}", buf);
-
 	for i in 1..2018 {
 		let l = buf.len();
 
 		p = (p + s) % l;
 
-		println!("{} {}", p, l);
+//		println!("{} {}", p, l);
 
 		buf.insert(p + 1, i);
 
 		p = p + 1;
 
-		println!("{:?}", buf);
+//		println!("{:?}", buf);
 	}
+
+	println!("{:?}", buf.get(p - 3 .. p + 4).unwrap());
 }
