8b0ce5ecec1498ce87804ce41afda23ca6c76f0c
Edward Cragg <drq.11235@gmail.com>
2017-12-17 19:46:30 +0000

    Update rust_template.sh

--- rust_template.sh
+++ rust_template.sh
@@ -2,18 +2,20 @@
 #
 # Create rust template
 
-if [[ $# -ne 2 ]]; then
-	printf "usage:\n    $0 [name] [day-number]\n"
+d=$(basename $PWD)
+
+if [[ $# -ne 1 ]]; then
+	printf "usage:\n    $0 [name]\n"
 	exit 1
 fi
 
-echo "Creating empty rust project \"$1\" for day \"$2\""
+echo "Creating empty rust project \"$1\" for day $d"
 
 cargo new "$1" --bin
 
 cat > "./$1/src/main.rs" << EOF
 /* Advent of code */
-/* day $1 */
+/* day $d */
 
 fn main () {
 	part1();
@@ -30,11 +32,11 @@ fn part2() {
 EOF
 
 echo '.*.sw*' > .gitignore
-echo "$1" >> .gitignore
 echo ".git_template" >> .gitignore
 
 cat > Makefile << EOF
-day=$2
+day=$d
+target=debug
 
 debug_target=./$1/target/debug/$1
 release_target=./$1/target/release/$1
@@ -53,17 +55,19 @@ debug: \$(debug_target)
 release: \$(release_target)
 	\$(release_target)
 
-$1: release
+$1: \$(target)
 
 all: $1
-	./$1
+
+vi:
+	vi \$(src)
 
 .git_template:
 	echo "\$(day): " > .git_template
 
 commit: .git_template
-	git commit -t .git_template
+	git commit -st .git_template
 EOF
 
 git add Makefile "$1" .gitignore
-git ct -m "$2: add rust template"
+git ct -m "$d: add rust template"
