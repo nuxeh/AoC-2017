9345ffe14cab1cf95065bf92c7729cc861aa645a
Edward Cragg <drq.11235@gmail.com>
2017-12-17 14:53:09 +0000

    17: add spinlock source to cargo

--- 17/spin_lock/src/main.rs
+++ 17/spin_lock/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+fn main () {
+
+	let test = false;
+	let part = 2;
+	let s; if test { s = 3 } else { s = 324 }
+	let t; if part == 1 { t = 2018 } else { t = 50000001 }
+
+	let mut buf = vec![0];
+	let mut p = 0;
+
+	for i in 1..t {
+		let l = buf.len();
+
+		p = (p + s) % l;
+
+//		println!("{} {}", p, l);
+
+		buf.insert(p + 1, i);
+
+		p = p + 1;
+
+//		println!("{:?}", buf);
+	}
+
+	println!("{:?}", buf.get(p - 3 .. p + 4).unwrap());
+	println!("final value is {}", buf[p +1]);
+	println!("value after 0 is {}", buf[buf.binary_search(&0).unwrap()+1]);
+
 }
