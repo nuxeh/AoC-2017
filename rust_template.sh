9c9dac3240d4979cff49fd96f96b50cbe8af5388
Edward Cragg <drq.11235@gmail.com>
2017-12-17 19:25:01 +0000

    Update rust_template.sh

--- rust_template.sh
+++ rust_template.sh
@@ -7,8 +7,24 @@ if [[ $# -ne 2 ]]; then
 	exit 1
 fi
 
-cat > "$1.rs" << EOF
+echo "Creating empty rust project \"$1\" for day \"$2\""
+
+cargo new "$1" --bin
+
+cat > "./$1/src/main.rs" << EOF
+/* Advent of code */
+/* day $1 */
+
 fn main () {
+	part1();
+	part2();
+}
+
+fn part1() {
+
+}
+
+fn part2() {
 
 }
 EOF
@@ -20,8 +36,24 @@ echo ".git_template" >> .gitignore
 cat > Makefile << EOF
 day=$2
 
-$1: $1.rs
-	rustc \$@.rs
+debug_target=./$1/target/debug/$1
+release_target=./$1/target/release/$1
+
+src=./$1/src/main.rs
+
+\$(debug_target): \$(src)
+	cd $1 && cargo build
+
+\$(release_target): \$(src)
+	cd $1 && cargo build --release
+
+debug: \$(debug_target)
+	\$(debug_target)
+
+release: \$(release_target)
+	\$(release_target)
+
+$1: release
 
 all: $1
 	./$1
@@ -33,5 +65,5 @@ commit: .git_template
 	git commit -t .git_template
 EOF
 
-git add Makefile $1.rs .gitignore
+git add Makefile "$1" .gitignore
 git ct -m "$2: add rust template"
