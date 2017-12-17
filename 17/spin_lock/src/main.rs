5fa32dbca4b9e66ff85f9a7d04b0f5eeb9822bd6
Edward Cragg <drq.11235@gmail.com>
2017-12-17 18:14:12 +0000

    17: wip

--- 17/spin_lock/src/main.rs
+++ 17/spin_lock/src/main.rs
@@ -8,8 +8,8 @@ use pbr::ProgressBar;
 
 fn main () {
 
-	let test = true;
-	let part = 1;
+	let test = false;
+	let part = 2;
 	let s; if test { s = 3 } else { s = 324 }
 	let t; if part == 1 { t = 2018 } else { t = 50000001 }
 
@@ -19,11 +19,11 @@ fn main () {
 	let mut buf = vec![0];
 	let mut p = 0;
 
-	for i in 1..t {
+	for i in 1..100 {
 		let l = buf.len();
 
 //		p = (p + s) % l;
-asd
+
 		buf.rotate(s % l);
 
 //		println!("{} {}", p, l);
@@ -39,7 +39,8 @@ asd
 	}
 
 //	println!("{:?}", buf.get(p - 3 .. p + 4).unwrap());
+	println!("{:?}", buf);
 	println!("final value is {}", buf[0]);
-	println!("value after 0 is {}", buf[buf.binary_search(&8).unwrap()+1]);
+	println!("value after 0 is {}", buf[buf.binary_search(&0).unwrap()]);
 
 }
