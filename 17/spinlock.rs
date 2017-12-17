9a6dd69b465eabce8663d73077e04d47b315da75
Edward Cragg <drq.11235@gmail.com>
2017-12-17 13:42:06 +0000

    17: initial implementation

--- 17/spinlock.rs
+++ 17/spinlock.rs
@@ -1,3 +1,26 @@
 fn main () {
 
+	let test = true;
+	let s; if test { s = 3 } else { s = 324 }
+
+	let mut buf = vec![0];
+	let mut p = 0;
+
+	println!("{:?}", buf);
+
+	for i in 1..2018 {
+		let l = buf.len();
+
+		p = (p + s) % l;
+
+		println!("{} {}", p, l);
+
+		if p == l-1 {
+			buf.push(i);
+		} else {
+			buf[p+1] = i;
+		}
+
+		println!("{:?}", buf);
+	}
 }
