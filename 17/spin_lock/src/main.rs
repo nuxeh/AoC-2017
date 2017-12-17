85bb8dfb6c5a431d9ef5d316a5403fec9b39462b
Edward Cragg <drq.11235@gmail.com>
2017-12-17 16:15:22 +0000

    17: wip part 2 rotate

--- 17/spin_lock/src/main.rs
+++ 17/spin_lock/src/main.rs
@@ -1,15 +1,19 @@
+#![feature(slice_rotate)]
+// rustup install nightly
+// cargo +nightly build
+
 extern crate pbr;
 
 use pbr::ProgressBar;
 
 fn main () {
 
-	let test = false;
-	let part = 2;
+	let test = true;
+	let part = 1;
 	let s; if test { s = 3 } else { s = 324 }
 	let t; if part == 1 { t = 2018 } else { t = 50000001 }
 
-	let mut pb = ProgressBar::new(t / 1000);
+	let mut pb = ProgressBar::new(t);
 	pb.format("|▓░░|");
 
 	let mut buf = vec![0];
@@ -18,21 +22,24 @@ fn main () {
 	for i in 1..t {
 		let l = buf.len();
 
-		p = (p + s) % l;
+//		p = (p + s) % l;
+asd
+		buf.rotate(s % l);
 
 //		println!("{} {}", p, l);
 
-		buf.insert(p + 1, i);
+//		buf.insert(p + 1, i);
+		buf.push(i);
 
-		p = p + 1;
+//		p = p + 1;
 
-//		println!("{:?}", buf);
+		println!("{:?}", buf);
 
 		if i % 1000 == 0 { pb.inc(); }
 	}
 
-	println!("{:?}", buf.get(p - 3 .. p + 4).unwrap());
-	println!("final value is {}", buf[p +1]);
-	println!("value after 0 is {}", buf[buf.binary_search(&0).unwrap()+1]);
+//	println!("{:?}", buf.get(p - 3 .. p + 4).unwrap());
+	println!("final value is {}", buf[0]);
+	println!("value after 0 is {}", buf[buf.binary_search(&8).unwrap()+1]);
 
 }
