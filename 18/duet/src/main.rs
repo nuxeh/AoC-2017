de852b3906016b1861257ef7425e517512793819
Edward Cragg <drq.11235@gmail.com>
2017-12-18 10:13:17 +0000

    18: parse input from stdin into tuples
    Signed-off-by: Edward Cragg <drq.11235@gmail.com>

--- 18/duet/src/main.rs
+++ 18/duet/src/main.rs
@@ -7,15 +7,33 @@ use std::io::BufRead;
 fn main () {
 
 	let stdin = io::stdin();
-//	let ins: Vec<str> = stdin.lock().lines().collect();
 
-	println!("{:?}", stdin.lock().lines());
+	let mut p: Vec<(char, char, i32, String)> = vec![];
 
-	part1();
+	for line in stdin.lock().lines() {
+		let line = line.unwrap();
+		let split: Vec<_> = line.split(" ").collect();
+
+		let n: i32;
+		let mut r: char = '\0';
+		if split.len() <= 2 {n = 0} else {
+			n = split[2].parse().unwrap_or(0);
+			r = split[2].parse().unwrap_or('\0');
+		}
+
+		p.push((split[1].chars().nth(0).unwrap(),
+			r,
+			n,
+			split[0].to_string()));
+	}
+
+	part1(&p);
 	part2();
 }
 
-fn part1() {
+fn part1(p: &Vec<(char, char, i32, String)>) {
+
+	println!("{:?}", p);
 
 }
 
