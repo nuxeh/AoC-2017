e4a970bb12774ee5d8a8f285162697c7f42e1858
Edward Cragg <drq.11235@gmail.com>
2017-12-18 09:53:42 +0000

    18: initial attempt at stdin read
    Signed-off-by: Edward Cragg <drq.11235@gmail.com>

--- 18/duet/src/main.rs
+++ 18/duet/src/main.rs
@@ -1,7 +1,16 @@
 /* Advent of code */
 /* day 18 */
 
+use std::io;
+use std::io::BufRead;
+
 fn main () {
+
+	let stdin = io::stdin();
+//	let ins: Vec<str> = stdin.lock().lines().collect();
+
+	println!("{:?}", stdin.lock().lines());
+
 	part1();
 	part2();
 }
