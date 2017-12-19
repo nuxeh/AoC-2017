754c5c59be2867e36c2eadbfe8eae6ce4df30127
Edward Cragg <drq.11235@gmail.com>
2017-12-19 11:13:33 +0000

    19: file reading
    Signed-off-by: Edward Cragg <drq.11235@gmail.com>

--- 19/path/src/main.rs
+++ 19/path/src/main.rs
@@ -1,7 +1,26 @@
 /* Advent of code */
 /* day 19 */
 
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main () {
+
+	let test = true;
+
+	let filename;
+	if test {filename = "test.txt";} else {filename = "iput.txt";}
+
+	let mut f = File::open(filename).expect("file not found");
+
+	let mut input = String::new();
+	f.read_to_string(&mut input)
+		.expect("error reading file");
+
+	let split = input.split(|c| c == '\n');
+
+	let map = vec![vec![0; 200]; 200];
+
 	part1();
 	part2();
 }
