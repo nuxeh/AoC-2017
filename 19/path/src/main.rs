a8f3639ea09d18f77d8cdc7f381983c2a7692c3e
Edward Cragg <drq.11235@gmail.com>
2017-12-19 11:24:12 +0000

    19: stdin parse
    Signed-off-by: Edward Cragg <drq.11235@gmail.com>

--- 19/path/src/main.rs
+++ 19/path/src/main.rs
@@ -3,20 +3,25 @@
 
 use std::fs::File;
 use std::io::prelude::*;
+use std::io;
+use std::io::BufRead;
 
-fn read_from_file(filename: &String) {
-
-	let mut f = File::open(filename).expect("file not found");
-
-	let mut input = String::new();
-	f.read_to_string(&mut input)
-		.expect("error reading file");
-
-	let _lines = input.split(|c| c == '\n');
-
-}
 
 fn main () {
+
+	let stdin = io::stdin();
+
+	for line in stdin.lock().lines() {
+		let line = line.unwrap();
+		let split: Vec<_> = line.split("").collect();
+
+		/* std::vec::Vec<&str> */
+
+		println!("{:?}", split);
+
+		//let a: i32 = split;
+	}
+
 
 	let map = vec![vec![0; 200]; 200];
 
@@ -29,5 +34,17 @@ fn part1() {
 }
 
 fn part2() {
+
+}
+
+fn read_from_file(filename: &String) {
+
+	let mut f = File::open(filename).expect("file not found");
+
+	let mut input = String::new();
+	f.read_to_string(&mut input)
+		.expect("error reading file");
+
+	let _lines = input.split(|c| c == '\n');
 
 }
