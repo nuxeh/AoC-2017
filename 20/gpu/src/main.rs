a2f30368fdfd97999ee30cd2837fd44e06bdf65b
Edward Cragg <drq.11235@gmail.com>
2017-12-20 10:57:03 +0000

    20: start stdin read, instructions struct
    Signed-off-by: Edward Cragg <drq.11235@gmail.com>

--- 20/gpu/src/main.rs
+++ 20/gpu/src/main.rs
@@ -1,9 +1,32 @@
 /* Advent of code */
 /* day 20 */
 
+use std::io;
+use std::io::BufRead;
+
+#[derive(Debug)]
+struct I {
+	p: (u32, u32, u32),
+	v: (u32, u32, u32),
+	a: (u32, u32, u32)
+}
+
 fn main () {
 	part1();
 	part2();
+}
+
+fn read_stdin() {
+
+	let stdin = io::stdin();
+
+	let mut map: Vec<Vec<&str>> = vec![];
+
+	for line in stdin.lock().lines() {
+		let l = line.unwrap().to_string();
+		let split: Vec<_> = l.split("").collect();
+	}
+
 }
 
 fn part1() {
