36c176cd3a98133e44d28970c953d2e9cdc1e694
Edward Cragg <drq.11235@gmail.com>
2017-12-21 11:02:40 +0000

    21: squash some errors
    Signed-off-by: Edward Cragg <drq.11235@gmail.com>

--- 21/picture/src/main.rs
+++ 21/picture/src/main.rs
@@ -7,17 +7,19 @@ use std::fmt;
 
 #[derive(Clone)]
 struct Pic {
-	b: Vec<u32>,
+	b: Vec<bool>,
+	r: Vec<Vec<bool>>,
 	w: usize,
 	h: usize,
 }
 
 impl fmt::Debug for Pic {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-	for y in 0..h {
+	for y in 0..self.h {
 	       write!(f, "Point {{ x: {}, y: {} }}", self.w, self.h)
 	}
     }
+	Ok(1)
 }
 
 impl Pic {
@@ -26,22 +28,22 @@ impl Pic {
 		let mut w;
 		let mut h;
 
-		let lines = st.split("/").collect();
+		let lines = s.split("/").collect();
 		for l in lines {
-			
+
 		}
 
-		w = split[0].len();
-		h = split.len();
+		w = lines[0].len();
+		h = lines.len();
 
-		Pic {w: w, h: h, b: }}
+		Pic {w: w, h: h, b: }
 	}
 
 }
 
 fn main () {
 
-	let rs: Vec<P> = read_stdin();
+	let rs: Vec<(Pic, Pic)> = read_stdin();
 
 //	println!("{:?}", rs);
 
@@ -58,40 +60,40 @@ fn part2() {
 
 }
 
-fn read_stdin() -> Vec<P> {
+fn read_stdin() -> Vec<(Pic, Pic)> {
 
 	let stdin = io::stdin();
-	let mut v: Vec<P> = vec![];
+	let mut v: Vec<(Pic, Pic)> = vec![];
 
 	for line in stdin.lock().lines() {
-		let mut i: P = P {
-			a: Xyz {x: 0, y: 0, z: 0},
-			p: Xyz {x: 0, y: 0, z: 0},
-			v: Xyz {x: 0, y: 0, z: 0},
-			collided: false};
-
-		let l = line.unwrap().to_string();
-		let s: Vec<_> = l.split(", ").collect();
-
-		for (n, j) in s.iter().enumerate() {
-			let s2: Vec<_> = j.split(|c| c == '<' ||
-						     c == '>' ||
-						     c == ',').collect();
-
-			let x: i64 = s2[1].parse().unwrap();
-			let y: i64 = s2[2].parse().unwrap();
-			let z: i64 = s2[3].parse().unwrap();
-
-			match n {
-				0 => i.p = Xyz {x: x, y: y, z: z},
-				1 => i.v = Xyz {x: x, y: y, z: z},
-				2 => i.a = Xyz {x: x, y: y, z: z},
-				_ => {}
-			}
-		}
-
-		println!("{:?}", i);
-		v.push(i);
+//		let mut i: P = P {
+//			a: Xyz {x: 0, y: 0, z: 0},
+//			p: Xyz {x: 0, y: 0, z: 0},
+//			v: Xyz {x: 0, y: 0, z: 0},
+//			collided: false};
+//
+//		let l = line.unwrap().to_string();
+//		let s: Vec<_> = l.split(", ").collect();
+//
+//		for (n, j) in s.iter().enumerate() {
+//			let s2: Vec<_> = j.split(|c| c == '<' ||
+//						     c == '>' ||
+//						     c == ',').collect();
+//
+//			let x: i64 = s2[1].parse().unwrap();
+//			let y: i64 = s2[2].parse().unwrap();
+//			let z: i64 = s2[3].parse().unwrap();
+//
+//			match n {
+//				0 => i.p = Xyz {x: x, y: y, z: z},
+//				1 => i.v = Xyz {x: x, y: y, z: z},
+//				2 => i.a = Xyz {x: x, y: y, z: z},
+//				_ => {}
+//			}
+//		}
+//
+//		println!("{:?}", i);
+//		v.push(i);
 	}
 
 	v
