b273c7e551f530712693cacbdd6f62c1f0350d27
Edward Cragg <drq.11235@gmail.com>
2017-12-21 11:31:34 +0000

    21: create struct from string and parse standard input
    Signed-off-by: Edward Cragg <drq.11235@gmail.com>

--- 21/picture/src/main.rs
+++ 21/picture/src/main.rs
@@ -6,37 +6,44 @@ use std::io::BufRead;
 use std::fmt;
 
 #[derive(Clone)]
+#[derive(Debug)]
 struct Pic {
 	b: Vec<bool>,
-	r: Vec<Vec<bool>>,
+//	r: Vec<Vec<bool>>,
 	w: usize,
 	h: usize,
 }
 
-impl fmt::Debug for Pic {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-	for y in 0..self.h {
-	       write!(f, "Point {{ x: {}, y: {} }}", self.w, self.h)
-	}
-    }
-	Ok(1)
-}
+//impl fmt::Debug for Pic {
+//    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//	for y in 0..self.h {
+//	       write!(f, "Point {{ x: {}, y: {} }}", self.w, self.h)
+//	}
+//    }
+//}
 
 impl Pic {
 
-	fn new_from_string(s: &String) -> Pic {
+	fn new_from_string(s: String) -> Pic {
 		let mut w;
 		let mut h;
+		let mut b = vec![];
 
-		let lines = s.split("/").collect();
-		for l in lines {
-
-		}
+		let lines: Vec<_> = s.split("/").collect();
 
 		w = lines[0].len();
 		h = lines.len();
 
-		Pic {w: w, h: h, b: }
+		for l in lines {
+			for c in l
+			.chars()
+			.map(|c| if c == '#' {true} else {false}) {
+				println!("{}", c);
+				b.push(c);
+			}
+		}
+
+		Pic {w: w, h: h, b: b}
 	}
 
 }
@@ -45,7 +52,7 @@ fn main () {
 
 	let rs: Vec<(Pic, Pic)> = read_stdin();
 
-//	println!("{:?}", rs);
+	println!("{:?}", rs);
 
 	let mut rs1 = rs.clone();
 
@@ -66,34 +73,12 @@ fn read_stdin() -> Vec<(Pic, Pic)> {
 	let mut v: Vec<(Pic, Pic)> = vec![];
 
 	for line in stdin.lock().lines() {
-//		let mut i: P = P {
-//			a: Xyz {x: 0, y: 0, z: 0},
-//			p: Xyz {x: 0, y: 0, z: 0},
-//			v: Xyz {x: 0, y: 0, z: 0},
-//			collided: false};
-//
-//		let l = line.unwrap().to_string();
-//		let s: Vec<_> = l.split(", ").collect();
-//
-//		for (n, j) in s.iter().enumerate() {
-//			let s2: Vec<_> = j.split(|c| c == '<' ||
-//						     c == '>' ||
-//						     c == ',').collect();
-//
-//			let x: i64 = s2[1].parse().unwrap();
-//			let y: i64 = s2[2].parse().unwrap();
-//			let z: i64 = s2[3].parse().unwrap();
-//
-//			match n {
-//				0 => i.p = Xyz {x: x, y: y, z: z},
-//				1 => i.v = Xyz {x: x, y: y, z: z},
-//				2 => i.a = Xyz {x: x, y: y, z: z},
-//				_ => {}
-//			}
-//		}
-//
-//		println!("{:?}", i);
-//		v.push(i);
+		let l = line.unwrap().to_string();
+		let s: Vec<_> = l.split(" => ").collect();
+
+		println!("{} {:?}", l, v);
+		v.push((Pic::new_from_string(s[0].to_string()),
+			Pic::new_from_string(s[1].to_string())));
 	}
 
 	v
