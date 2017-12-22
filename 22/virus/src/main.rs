c608135e33cc22ee63ec519a17525f14a468b0f5
Edward Cragg <drq.11235@gmail.com>
2017-12-22 10:19:49 +0000

    22: wip
    Signed-off-by: Edward Cragg <drq.11235@gmail.com>

--- 22/virus/src/main.rs
+++ 22/virus/src/main.rs
@@ -1,6 +1,15 @@
 /* Advent of code */
 /* day 22 */
 
+use std::io;
+use std::io::BufRead;
+use std::collections::HashMap;
+
+struct Xy {
+	x: u64, // infinite, yeah!
+	y: u64
+}
+
 fn main () {
 	part1();
 	part2();
@@ -12,4 +21,17 @@ fn part1() {
 
 fn part2() {
 
+}
+
+fn read_stdin(map: &mut HashMap<Xy, bool>) {
+
+	let stdin = io::stdin();
+
+	for (y, line) in stdin.lock().lines().enumerate() {
+		let l = line.unwrap().to_string();
+
+		for (x, c) in l.chars().enumerate() {
+			map.insert(Xy {x: x, y: y});
+		}
+	}
 }
