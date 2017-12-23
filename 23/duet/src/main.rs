1e027031ae989c6f1957bc23c61007d95f9c833a
Edward Cragg <drq.11235@gmail.com>
2017-12-23 16:19:44 +0000

    23: modify day 18 for today's problem
    Signed-off-by: Edward Cragg <drq.11235@gmail.com>

--- 23/duet/src/main.rs
+++ 23/duet/src/main.rs
@@ -1,11 +1,9 @@
 /* Advent of code */
-/* day 18 */
+/* day 23 */
 
 use std::io;
 use std::io::BufRead;
 use std::collections::HashMap;
-//use std::thread;
-use std::sync::mpsc::channel;
 
 fn main () {
 
@@ -49,9 +47,6 @@ fn main () {
 	}
 
 	part1(&mut p, &mut rs);
-	println!("last written frequency is {}", rs["answer"]);
-	println!("part 2 ............");
-	part2(&mut p, &mut rs2_0, &mut rs2_1);
 }
 
 fn inst(i: &(char, char, i64, i32, String), rs: &mut HashMap<String, i64>) -> i8 {
@@ -78,37 +73,16 @@ fn inst(i: &(char, char, i64, i32, String), rs: &mut HashMap<String, i64>) -> i8
 
 	match i.4.as_ref() { // to slice
 
-		"snd" => {
-				rs.insert("snd".to_string(), rv);
-				ret = -1;
-			}
-
-		"rcv" => {
-				if rv != 0 {
-					if rs.contains_key("snd") {
-//						println!("rcv is {}", rs["snd"]);
-						let r = rs["snd"];
-						rs.insert("answer".to_string(), r);
-					}
-				}
-
-				if rs["wait"] == 0 {
-					let rcv = *rs.entry("rcv".to_string()).or_insert(0);
-					rs.insert(r, rcv);
-					rs.insert("wait".to_string(), 1);
-				} else {
-					return -2;
-				}
-			}
-
 		"set" => {rs.insert(r, v);}
-		"add" => {rs.insert(r, rv + v);}
-		"mul" => {rs.insert(r, rv * v);}
-		"mod" => {rs.insert(r, rv % v);}
-
-		"jgz" => {if rv > 0  {
+		"mul" => {
+				rs.insert(r, rv * v);
+				*rs.entry("mulcnt".to_string()).or_insert(0) += 1;
+			}
+		"sub" => {rs.insert(r, rv - v);}
+		"jnz" => {if rv > 0  {
 			jump = true;
 			rs.insert("pc".to_string(), pc + v)} else {None};
+			println!("rv = {}", rv);
 		}
 
 		_     => {println!("Unknown instruction {} !", i.4);}
@@ -121,92 +95,20 @@ fn inst(i: &(char, char, i64, i32, String), rs: &mut HashMap<String, i64>) -> i8
 
 fn part1(p: &Vec<(char, char, i64, i32, String)>, mut rs: &mut HashMap<String, i64>) {
 
-//	println!("{:?}", p);
-
-	// rs["pc"] = 0; // doesn't work
+	println!("{:#?}", p);
 	rs.insert("pc".to_string(), 0);
-	rs.insert("wait".to_string(), 0);
 
 	loop {
 		let pc = rs["pc"] as usize;
 
-//		println!("{:?}", p[pc]);
+		println!("{:?}", p[pc]);
 
 		if inst(&p[pc], &mut rs) == -2 {break;}
 
-//		println!("{:?}", rs);
+		println!("{:?}", rs);
 
 		if rs["pc"] >= p.len() as i64 {break;}
 	}
-
-//	println!("{:?}", rs);
-}
-
-fn part2(p: &Vec<(char, char, i64, i32, String)>,
-	 mut rs: &mut HashMap<String, i64>,
-	 mut rs1: &mut HashMap<String, i64>) {
-
-	let (tx, rx) = channel();
-	let (tx2, rx2) = channel();
-
-	rs.insert("pc".to_string(), 0);
-	rs1.insert("pc".to_string(), 0);
-	rs.insert("wait".to_string(), 1);
-	rs1.insert("wait".to_string(), 1);
-
-//	run(&mut p, &mut rs);
-
-	let mut send_count_1 = 0;
-
-	loop {
-		let pc = rs["pc"] as usize;
-		let pc1 = rs1["pc"] as usize;
-
-		match inst(&p[pc], &mut rs) {
-			-1 => {
-				let s = rs["snd"];
-				let _ = tx.send(s);
-//				println!("0 sending {}", s);
-			}
-			-2 => {
-				let m = rx2.try_recv();
-				if ! m.is_err() {
-					let r = m.unwrap();
-//					println!("0 received {}", r);
-					rs.insert("rcv".to_string(), r);
-					rs.insert("wait".to_string(), 0);
-				}
-			}
-			_  => {}
-		}
-
-//		println!("0: {:?}", rs);
-
-		match inst(&p[pc1], &mut rs1) {
-			-1 => {
-				let s = rs1["snd"];
-				let _ = tx2.send(s);
-				send_count_1 += 1;
-				println!("1 sending {} [{}]", s, send_count_1);
-			}
-			-2 => {
-				let m = rx.try_recv();
-				if ! m.is_err() {
-					let r = m.unwrap();
-//					println!("1 received {}", r);
-					rs1.insert("rcv".to_string(), r);
-					rs1.insert("wait".to_string(), 0);
-				}
-			}
-			_  => {}
-		}
-
-//		println!("1: {:?}", rs1);
-
-		if rs["pc"] >= p.len() as i64 {break;}
-		if rs1["pc"] >= p.len() as i64 {break;}
-	}
-
 }
 
 fn run(p: &Vec<(char, char, i64, i32, String)>, mut rs: &mut HashMap<String, i64>) {
