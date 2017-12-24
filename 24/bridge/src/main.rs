e70c24755a326b096552e2e76ff6a40d6d5e8120
Edward Cragg <drq.11235@gmail.com>
2017-12-24 23:23:14 +0000

    24: wip
    Signed-off-by: Edward Cragg <drq.11235@gmail.com>

--- 24/bridge/src/main.rs
+++ 24/bridge/src/main.rs
@@ -3,6 +3,7 @@
 
 use std::io;
 use std::io::BufRead;
+use std::collections::HashMap;
 
 fn main () {
 	let v = read_stdin();
@@ -13,11 +14,12 @@ fn main () {
 
 fn part1(blocks: &Vec<Vec<u32>>) {
 
-	let mut bridges: Vec<Vec<u32>> = vec![vec![]];
+	let mut bridges: HashMap<usize, Vec<u32>> = HashMap::new();
 	let mut n = 0;
 
 	for (startpos, block) in blocks.iter().enumerate() {
 		if block.contains(&0) {
+			bridges.insert(startpos, vec![]);
 			walk(startpos, n, blocks, &mut bridges);
 			n += 1;
 		}
@@ -27,10 +29,8 @@ fn part1(blocks: &Vec<Vec<u32>>) {
 fn walk(s: usize,
 	n: usize,
 	blocks: &Vec<Vec<u32>>,
-	mut bridges: &mut Vec<Vec<u32>>
+	mut bridges: &mut HashMap<usize, Vec<u32>>
 	) {
-
-	let b = bridges.entry(n).or_insert(vec![]);
 
 	for (startpos, _) in blocks
 	.iter()
