c87878941df7f48a2cb9c6a56a25684c51a1620f
Edward Cragg <drq.11235@gmail.com>
2017-12-25 23:04:14 +0000

    25: add Tape struct
    Signed-off-by: Edward Cragg <drq.11235@gmail.com>

--- 25/turing/src/main.rs
+++ 25/turing/src/main.rs
@@ -1,8 +1,28 @@
 /* Advent of code */
 /* day 25 */
 
+use std::io;
+use std::io::BufRead;
+
+struct Tape {
+	start: usize,
+	end: usize,
+	vec: Vec<bool>
+}
+
+impl Tape {
+	fn new() -> Tape {Tape {start: 0, end: 0, vec: vec![false]}}
+
+	fn move_left(&mut self) {
+
+	}
+}
+
+
 fn main () {
 	read_stdin();
+
+	let mut tape = Tape::new();
 
 	part1();
 	part2();
