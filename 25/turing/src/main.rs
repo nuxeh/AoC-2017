954eb8908b11e00868907e7820e8d01616761fe0
Edward Cragg <drq.11235@gmail.com>
2017-12-25 23:23:51 +0000

    25: add move left and move right traits
    Signed-off-by: Edward Cragg <drq.11235@gmail.com>

--- 25/turing/src/main.rs
+++ 25/turing/src/main.rs
@@ -5,19 +5,31 @@ use std::io;
 use std::io::BufRead;
 
 struct Tape {
-	start: usize,
 	end: usize,
+	pos: usize,
 	vec: Vec<bool>
 }
 
 impl Tape {
-	fn new() -> Tape {Tape {start: 0, end: 0, vec: vec![false]}}
+	fn new() -> Tape {Tape {end: 0, pos: 0, vec: vec![false]}}
+
+	fn move_right(&mut self) {
+		self.pos += 1;
+		if self.pos > self.end {
+			self.vec.push(false);
+			self.end += 1;
+		}
+	}
 
 	fn move_left(&mut self) {
-
+		if self.pos > 0 {
+			self.pos -= 1;
+		} else {
+			self.vec.insert(0, false);
+			self.end += 1;
+		}
 	}
 }
-
 
 fn main () {
 	read_stdin();
