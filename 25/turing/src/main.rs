c0a5bb40a2e852148ab8b239c5fb0085c4adad60
Edward Cragg <drq.11235@gmail.com>
2017-12-25 22:47:36 +0000

    25: add rust template

---
+++ 25/turing/src/main.rs
@@ -0,0 +1,27 @@
+/* Advent of code */
+/* day 25 */
+
+fn main () {
+	read_stdin();
+
+	part1();
+	part2();
+}
+
+fn part1() {
+
+}
+
+fn part2() {
+
+}
+
+fn read_stdin() {
+	let std = io::stdin();
+
+	for l in std.lock().lines() {
+		let l = l.unwrap();
+		println!("{}", l);
+
+	}
+}
