1f0d575510b0f4a4615bc8aa6c0bb78b566bdd4a
Edward Cragg <drq.11235@gmail.com>
2017-12-26 14:25:39 +0000

    update loc.sh

--- loc.sh
+++ loc.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-if ! which cloc; then
+basedir=$(pwd -P | sed 's/\(AoC_20..\).*$/\1/')
+
+if ! which cloc &> /dev/null; then
 	echo "\`cloc\` not installed, installing..."
 	sudo apt -y install cloc
-else
+fi
+
+printf "\n$PWD\n\n"
+if ! [[ "$PWD" == "$basedir" ]]; then
 	cloc .
 fi
+
+printf "$basedir \n\n"
+cloc $basedir
